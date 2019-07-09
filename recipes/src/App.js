14a00cded6a7cf802fbd35b204bce60f378440a9
godwinwabwile <wegowabz1@gmail.com>
2019-07-09 13:04:39 -0400

    changed the api call to use the local data

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -6,32 +6,32 @@ import './App.css';
 
 class App extends Component {
   state={
-    recipes:[],
+    recipes:recipes,
     url: "https://www.food2fork.com/api/search?key=871808d2890050df9f41ba685fc2108f"
   }
- //Ajax request
-  //method pulls data from the Api with the Async await
-  //method converts the dats into json format
-  //method changes the state of this.state.recipes 
-  async getRecipes(){
-    //catching ay possible errors during data fetching
-    try{ 
-      const Data = await fetch(this.state.url);//fetch data from api
-      const jsonData = await Data.json(); //convert the data to json
+//  //Ajax request
+//   //method pulls data from the Api with the Async await
+//   //method converts the dats into json format
+//   //method changes the state of this.state.recipes 
+//   async getRecipes(){
+//     //catching ay possible errors during data fetching
+//     try{ 
+//       const Data = await fetch(this.state.url);//fetch data from api
+//       const jsonData = await Data.json(); //convert the data to json
 
-      this.setState({
-        recipes:jsonData.recipes
-      })
+//       this.setState({
+//         recipes:jsonData.recipes
+//       })
 
-    }catch(error){
-      console.log(error);
-    }
-  }
-  componentDidMount(){
-    this.getRecipes();
-  }
+//     }catch(error){
+//       console.log(error);
+//     }
+//   }
+//   componentDidMount(){
+//     this.getRecipes();
+//   }
   render() {
-    console.log(this.state.recipes);
+    // console.log(this.state.recipes);
     return (
       <React.Fragment>
         <RecipeList/>
