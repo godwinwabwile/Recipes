27d7a93787fc7f415361285e2af375d5b32ab5ee
godwinwabwile <wegowabz1@gmail.com>
2019-07-10 02:50:08 -0400

    created the layout for the recipes in the Recipe.js component

--- recipes/src/components/Recipe.js
+++ recipes/src/components/Recipe.js
@@ -3,9 +3,42 @@ import React, {Component} from 'react';
 class Recipe extends Component{
 
     render(){
+       const{
+           image_url,
+           title,
+           publisher,
+           source_url,
+           recipe_id
+       } = this.props.recipe;
         return(
             <React.Fragment>
-                Hello from Recipes
+               <div className="col-10 mx-auto col-md-6 col-lg-4 my-3">
+                    <div className="card">
+                        <img
+                            src={image_url}
+                            className="img-card-top"
+                            style={{height:"14rem"}}
+                            alt="recipe"
+                         />
+                         <div className="card-body text-capitalize">
+                            <h6>{title}</h6>
+                            <h6 className="text-warning text-slanted">
+                                providec by {publisher}
+                            </h6>
+                         </div>
+                         <div className="card-footer">
+                            <button type="button" className="btn text-capitalize btn-primary ">
+                                Detaiils
+                            </button>
+                            <a href={source_url} 
+                            className="btn text-capitalize btn-success mx-2"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            >
+                                Recipe Url</a>
+                         </div>
+                    </div>
+               </div>
             </React.Fragment>
         )
     }
