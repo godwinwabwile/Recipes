623ab140a3ea43bc67cb391b088bd7b4084c8e66
godwinwabwile <wegowabz1@gmail.com>
2019-07-11 04:55:13 -0400

    added a search ui in the RecipeSearch component

--- recipes/src/components/RecipeSearch.js
+++ recipes/src/components/RecipeSearch.js
@@ -8,7 +8,34 @@ class RecipeSearch extends Component{
                 <div className="container">
                     <div className="row">
                         <div className="col-10 mx-auto col-md-8 mt-5 text-center">
-                            <h1 className="text-slanded text-capitalize"> search for food recipes with <strong>Food2Fork API</strong></h1>
+                            <h1 className="text-slanded text-capitalize"> 
+                            search for food recipes with: {" "}  
+                            <strong className="text-danger">Food2Fork API</strong>
+                            </h1>
+                            <form className="mt-4">
+                                <label htmlFor="search" 
+                                className="text-capitalize">
+                                    submit recipes separated by a comma
+                                </label>
+                                <div className="input-group">
+                                    <input 
+                                        type="text"
+                                        name="search"
+                                        className="form-control"
+                                        placeholder="onions,chicken,carrots"
+
+                                    />
+                                    <div className="input-group-append">
+                                        <button 
+                                            type="submit"
+                                            className="input-group-text bg-primary text-white text-slanded"
+                                        >
+                                           search
+                                        </button>
+                                    </div>
+
+                                </div>
+                            </form>
 
                         </div>
 
